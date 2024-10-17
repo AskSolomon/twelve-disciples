46a1f63a0137ccd56516d92925456459ee144aa7
doctorchase <doctorchase123@protonmail.com>
2024-10-17 23:57:55 +0530

    Update DisciplesGrid.jsx with Text

--- src/components/DisciplesGrid.jsx
+++ src/components/DisciplesGrid.jsx
@@ -3,18 +3,18 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 
 const disciples = [
-  { name: "Peter", description: "Also known as Simon Peter, he was a fisherman and one of Jesus' closest disciples." },
-  { name: "Andrew", description: "Peter's brother and also a fisherman. He was the first disciple Jesus called." },
-  { name: "James", description: "Son of Zebedee, he was part of Jesus' inner circle along with Peter and John." },
-  { name: "John", description: "Brother of James and known as the 'beloved disciple'. He wrote the Gospel of John." },
-  { name: "Philip", description: "From Bethsaida, he brought Nathanael to Jesus." },
-  { name: "Bartholomew", description: "Also known as Nathanael, he was brought to Jesus by Philip." },
-  { name: "Matthew", description: "A tax collector who became a disciple. He wrote the Gospel of Matthew." },
-  { name: "Thomas", description: "Known as 'Doubting Thomas' for his initial disbelief in Jesus' resurrection." },
-  { name: "James", description: "Son of Alphaeus, also known as James the Less." },
-  { name: "Thaddaeus", description: "Also known as Jude or Lebbaeus, son of James." },
-  { name: "Simon", description: "Called the Zealot, he was a member of a Jewish nationalist sect." },
-  { name: "Judas Iscariot", description: "The disciple who betrayed Jesus. He was later replaced by Matthias." },
+  { name: "Peter", description: "Is also known as Simon Peter, he was a fisherman and one of Jesus' closest disciples." },
+  { name: "Andrew", description: "Is Peter's brother and also a fisherman. He was the first disciple Jesus called." },
+  { name: "James", description: "Is Son of Zebedee, he was part of Jesus' inner circle along with Peter and John." },
+  { name: "John", description: "Is Brother of James and known as the 'beloved disciple'. He wrote the Gospel of John." },
+  { name: "Philip", description: "Is from Bethsaida, he brought Nathanael to Jesus." },
+  { name: "Bartholomew", description: "Is also known as Nathanael, he was brought to Jesus by Philip." },
+  { name: "Matthew", description: "Is A tax collector who became a disciple. He wrote the Gospel of Matthew." },
+  { name: "Thomas", description: "Is Known as 'Doubting Thomas' for his initial disbelief in Jesus' resurrection." },
+  { name: "James", description: "Is Son of Alphaeus, also known as James the Less." },
+  { name: "Thaddaeus", description: "Is also known as Jude or Lebbaeus, son of James." },
+  { name: "Simon", description: "Is called the Zealot, he was a member of a Jewish nationalist sect." },
+  { name: "Judas Iscariot", description: "Is the disciple who betrayed Jesus. He was later replaced by Matthias." },
 ];
 
 const DisciplesGrid = () => {
